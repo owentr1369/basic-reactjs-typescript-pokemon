b5c120eee3218b7e6704f9cd1f92a43320f16554
TranTam <tamvaa1306@gmail.com>
2022-07-12 14:20:46 +0700

    loadmore functionality

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,40 @@ interface Pokemons {
 
 const App: React.FC = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([])
+  const [nextUrl, setNextUrl] = useState<string>('')
+
   useEffect(() => {
     const getPokemon = async () => {
       const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20&offset=20')
+
+      setNextUrl(res.data.next)
+
       res.data.results.forEach(async (pokemon: Pokemons) => {
         const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        console.log('poke.data :>> ', poke.data);
+
         setPokemons((p) => [...p, poke.data])
       })
     }
     getPokemon()
   }, [])
 
+  const loadMore = async () => {
+    let res = await axios.get(nextUrl);
+    setNextUrl(res.data.next)
+    res.data.results.forEach(async (pokemon: Pokemons) => {
+      const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+      setPokemons((p) => [...p, poke.data])
+    })
+  }
+
   return (
     <div className="App">
       <div className="container">
         <header className="pokemon-header">Pokemon</header>
         <PokemonCollection pokemons={pokemons} />
+        <div className="btn">
+          <button onClick={loadMore}>Load More</button>
+        </div>
       </div>
     </div>
   );
